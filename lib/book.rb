677d95e7dc8355adbf2f69aa1e1ccbad1b41bd3d
Daria Approvato <dariaapprovato@gmail.com>
2023-01-02 21:48:47 -0500

    commit tree a887bbd69c2f7fd53ea168bfe5273bfc95a1aff3

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+require 'pry'
 
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    # def book(author, page_count, genre)
+    #     @author = author
+    #     @page_count = page
+    #     @genre = genre
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+# binding.pry
